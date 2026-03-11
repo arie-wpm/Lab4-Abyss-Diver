b976e52fe22a7ed2ce7604d8c9bc30c9edc8d55d
e2ik <erik.jensen.lin@gmail.com>
2026-03-11 11:42:02 +1100

    Oxygen Bubble Asset

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class BubbleScript : MonoBehaviour, IPickup
@@ -5,15 +6,33 @@ public class BubbleScript : MonoBehaviour, IPickup
     [SerializeField]
     [Range(1f, 10f)]
     private float oxygenGiven = 5f;
+    private Animator anim;
+    private CircleCollider2D col;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+        col = GetComponent<CircleCollider2D>();
+        if (anim == null) return;
+        anim.Play("BubbleMove", 0, Random.value);
+    }
 
     public void PlayerContact(PlayerStats player)
     {
         player.AddOxygen(oxygenGiven);
-        Destroy(gameObject);
+        if (anim == null) return;
+        anim.SetBool("isEmpty", true);
+        col.enabled = false;
+        StartCoroutine(DestoryAfterAnimation());
     }
 
     public void SetOxygenValue(float value)
     {
         oxygenGiven = Mathf.Min(value, 10);
+    }
+
+    IEnumerator DestoryAfterAnimation()
+    {
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(gameObject);
     }
 }
