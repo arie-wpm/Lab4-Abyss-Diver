b70c9542c7bf1d94fa2f24b76a8be8cb60f60911
Arie Maljers <ariemaljers@gmail.com>
2026-03-15 21:19:35 +1100

    SafeZones Respawn Bubbles at Interval

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using UnityEngine;
 
-public class BubbleScript : MonoBehaviour, IPickup {
+public class BubbleScript : MonoBehaviour, IPickup
+{
     [SerializeField, Range(1f, 10f)]
     private float oxygenGiven = 5f;
 
@@ -11,52 +12,70 @@ public class BubbleScript : MonoBehaviour, IPickup {
     private ParticleSystem[] particles;
     private Coroutine disableCoroutine;
 
-    void Awake() {
+    public bool isActive = true;
+
+    void Awake()
+    {
         anim = GetComponent<Animator>();
         col = GetComponent<CircleCollider2D>();
         sprites = GetComponentsInChildren<SpriteRenderer>();
         particles = GetComponentsInChildren<ParticleSystem>();
 
-        if (anim != null) anim.Play("BubbleMove", 0, Random.value);
+        if (anim != null)
+            anim.Play("BubbleMove", 0, Random.value);
     }
 
-    public void PlayerContact(PlayerStats player) {
+    public void PlayerContact(PlayerStats player)
+    {
         player.AddOxygen(oxygenGiven);
         player.isDrowning = false;
 
-        if (anim != null) anim.SetBool("isEmpty", true);
+        if (anim != null)
+            anim.SetBool("isEmpty", true);
 
         col.enabled = false;
+        isActive = false;
 
-        if (disableCoroutine != null) StopCoroutine(disableCoroutine);
+        if (disableCoroutine != null)
+            StopCoroutine(disableCoroutine);
         disableCoroutine = StartCoroutine(DisableAfterAnimation());
     }
 
-    IEnumerator DisableAfterAnimation() {
+    IEnumerator DisableAfterAnimation()
+    {
         yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
 
-        foreach (var sprite in sprites) sprite.enabled = false;
-        foreach (var ps in particles) ps.Stop();
+        foreach (var sprite in sprites)
+            sprite.enabled = false;
+        foreach (var ps in particles)
+            ps.Stop();
         disableCoroutine = null;
     }
 
-    public void SetOxygenValue(float value) {
+    public void SetOxygenValue(float value)
+    {
         oxygenGiven = Mathf.Min(value, 10);
     }
 
-    public void Reset() {
-        if (disableCoroutine != null) {
+    public void Reset()
+    {
+        if (disableCoroutine != null)
+        {
             StopCoroutine(disableCoroutine);
             disableCoroutine = null;
         }
 
-        if (anim != null) {
+        if (anim != null)
+        {
             anim.Rebind();
             anim.Play("BubbleMove", 0, Random.value);
         }
 
         col.enabled = true;
-        foreach (var sprite in sprites) sprite.enabled = true;
-        foreach (var ps in particles) ps.Play();
+        isActive = true;
+        foreach (var sprite in sprites)
+            sprite.enabled = true;
+        foreach (var ps in particles)
+            ps.Play();
     }
 }
