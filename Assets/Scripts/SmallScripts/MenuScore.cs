8bd3593a2a3baf3f47d0de2d1174909fa5dd4234
benhoweUTS <112325973+benhoweUTS@users.noreply.github.com>
2026-03-15 23:22:00 +1100

    menu highscore check

--- Assets/Scripts/SmallScripts/MenuScore.cs
+++ Assets/Scripts/SmallScripts/MenuScore.cs
@@ -6,11 +6,7 @@ public class MenuScore : MonoBehaviour
     private int highscore;
     void Start()
     {
-        if (PlayerPrefs.HasKey("HighScore"))
-        {
-            highscore = PlayerPrefs.GetInt("HighScore", 0);
-        }
-        
+        PlayerPrefs.GetFloat("HighScore", 0);
         var textBox = gameObject.GetComponent<TMP_Text>();
         textBox.text = "HighScore:\n" + highscore.ToString("00000");
 
